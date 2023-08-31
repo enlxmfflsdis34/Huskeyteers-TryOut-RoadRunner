6f433bc653aea498836a89f25b835d0a21e277d4
Christopher Hong <shong233@students.fairmontschools.com>
2023-09-01 07:18:11 +0900

    Inserted more detailed descriptions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TryoutOpmode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TryoutOpmode.java
@@ -35,7 +35,7 @@ public class TryoutOpmode extends LinearOpMode {
             double strafeInput = gamepad1.left_stick_x;
             double turnInput = gamepad1.right_stick_x;
 
-            // Power scaling modes
+            // Power scaling modes - Left Bumper (slow mode), Right bumper (Super Slow mode)
             if (gamepad1.left_bumper) {
                 driveInput *= 0.5;
                 strafeInput *= 0.5;
@@ -46,39 +46,40 @@ public class TryoutOpmode extends LinearOpMode {
                 turnInput *= 0.25;
             }
 
-            // Quick Turn
+            // Quick Turn - Turns twice faster, Press Right Trigger
+
             if (gamepad1.right_trigger > 0.5) {
                 turnInput *= 2.0;
             }
 
-            // Strafe Mode
+            // Strafe Mode - Makes easier to strafe, Press button B
             if (gamepad1.b) {
                 double robotAngle = drive.getPoseEstimate().getHeading();
                 strafeInput = 0.5 * Math.sin(robotAngle);  // The value "0.5" defines the speed and can be adjusted.
                 driveInput = 0.5 * Math.cos(robotAngle);
             }
 
-            // Point Turn Mode
+            // Point Turn Mode - Point turns, Press button X
             if (gamepad1.x) {
                 double desiredHeading = Math.atan2(-gamepad1.left_stick_y, gamepad1.left_stick_x);
                 double currentHeading = drive.getPoseEstimate().getHeading();
                 turnInput = desiredHeading - currentHeading;
             }
 
-            // Toggle field-oriented drive
+            // Toggle field-oriented drive - Press Button A
             if (gamepad1.a) {
                 fieldOriented = !fieldOriented;
                 while (gamepad1.a) {} // Debouncing
             }
 
-            // Brake Mode
+            // Brake Mode - Instantly brakes robot, Press Button Y
             if (gamepad1.y) {
                 driveInput = 0;
                 strafeInput = 0;
                 turnInput = 0;
             }
 
-            // Reset Odometry
+            // Reset Odometry - Reset the Odometry of Robot, Press Dpad Up
             if (gamepad1.dpad_up) {
                 drive.setPoseEstimate(new Pose2d(0, 0, 0));
             }
